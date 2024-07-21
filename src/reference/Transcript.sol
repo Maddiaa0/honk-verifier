b861d0027d2daa8a98f678739b598ac2277ea6ce
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-07-21 16:04:23 +0000

    feat: update to logderivative lookup

--- src/reference/Transcript.sol
+++ src/reference/Transcript.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-import {Honk, NUMBER_OF_ALPHAS, NUMBER_OF_ENTITIES, BATCHED_RELATION_PARTIAL_LENGTH} from "./HonkTypes.sol";
+import {Honk, NUMBER_OF_ALPHAS, NUMBER_OF_ENTITIES, BATCHED_RELATION_PARTIAL_LENGTH, CONST_PROOF_SIZE_LOG_N} from "./HonkTypes.sol";
 import {Fr, FrLib} from "./Fr.sol";
 import {LOG_N, NUMBER_OF_PUBLIC_INPUTS} from "./keys/EcdsaHonkVerificationKey.sol";
 
+// Transcript library to generate fiat shamir challenges
 struct Transcript {
     Fr eta;
+    Fr etaTwo;
+    Fr etaThree;
     Fr beta;
     Fr gamma;
     Fr[NUMBER_OF_ALPHAS] alphas;
     Fr[LOG_N] gateChallenges;
-    Fr[LOG_N] sumCheckUChallenges;
+    Fr[CONST_PROOF_SIZE_LOG_N] sumCheckUChallenges;
     Fr rho;
     // Zero morph
     Fr zmX;
@@ -20,25 +23,24 @@ struct Transcript {
     Fr zmQuotient;
     // Derived
     Fr publicInputsDelta;
-    Fr lookupGrandProductDelta;
 }
 
 library TranscriptLib {
-    function generateTranscript(
-        Honk.Proof memory proof,
-        Honk.VerificationKey memory vk,
-        bytes32[] calldata publicInputs
-    ) internal view returns (Transcript memory t) {
-        t.eta = generateEtaChallenge(proof, publicInputs);
+    function generateTranscript(Honk.Proof memory proof, Honk.VerificationKey memory vk, bytes32[] calldata publicInputs)
+        internal
+        view
+        returns (Transcript memory t)
+    {
+        (t.eta, t.etaTwo, t.etaThree) = generateEtaChallenge(proof, publicInputs);
 
-        (t.beta, t.gamma) = generateBetaAndGammaChallenges(t.eta, proof);
+        (t.beta, t.gamma) = generateBetaAndGammaChallenges(t.etaThree, proof);
 
         t.alphas = generateAlphaChallenges(t.gamma, proof);
 
         t.gateChallenges = generateGateChallenges(t.alphas[NUMBER_OF_ALPHAS - 1]);
 
         t.sumCheckUChallenges = generateSumcheckChallenges(proof, t.gateChallenges[LOG_N - 1]);
-        t.rho = generateRhoChallenge(proof, t.sumCheckUChallenges[LOG_N - 1]);
+        t.rho = generateRhoChallenge(proof, t.sumCheckUChallenges[CONST_PROOF_SIZE_LOG_N - 1]);
 
         t.zmY = generateZMYChallenge(t.rho, proof);
 
@@ -50,10 +52,8 @@ library TranscriptLib {
     function generateEtaChallenge(Honk.Proof memory proof, bytes32[] calldata publicInputs)
         internal
         view
-        returns (Fr eta)
+        returns (Fr eta, Fr etaTwo, Fr etaThree)
     {
-        // TODO(md): the 12 here will need to be halved when we fix the transcript to not be over field elements
-        // TODO: use assembly
         bytes32[3 + NUMBER_OF_PUBLIC_INPUTS + 12] memory round0;
         round0[0] = bytes32(proof.circuitSize);
         round0[1] = bytes32(proof.publicInputsSize);
@@ -64,7 +64,6 @@ library TranscriptLib {
 
         // Create the first challenge
         // Note: w4 is added to the challenge later on
-        // TODO: UPDATE ALL VALUES IN HERE
         round0[3 + NUMBER_OF_PUBLIC_INPUTS] = bytes32(proof.w1.x_0);
         round0[3 + NUMBER_OF_PUBLIC_INPUTS + 1] = bytes32(proof.w1.x_1);
         round0[3 + NUMBER_OF_PUBLIC_INPUTS + 2] = bytes32(proof.w1.y_0);
@@ -79,6 +78,8 @@ library TranscriptLib {
         round0[3 + NUMBER_OF_PUBLIC_INPUTS + 11] = bytes32(proof.w3.y_1);
 
         eta = FrLib.fromBytes32(keccak256(abi.encodePacked(round0)));
+        etaTwo = FrLib.fromBytes32(keccak256(abi.encodePacked(Fr.unwrap(eta))));
+        etaThree = FrLib.fromBytes32(keccak256(abi.encodePacked(Fr.unwrap(etaTwo))));
     }
 
     function generateBetaAndGammaChallenges(Fr previousChallenge, Honk.Proof memory proof)
@@ -86,17 +87,20 @@ library TranscriptLib {
         view
         returns (Fr beta, Fr gamma)
     {
-        // TODO(md): adjust round size when the proof points are generated correctly - 5
-        bytes32[9] memory round1;
+        bytes32[13] memory round1;
         round1[0] = FrLib.toBytes32(previousChallenge);
-        round1[1] = bytes32(proof.sortedAccum.x_0);
-        round1[2] = bytes32(proof.sortedAccum.x_1);
-        round1[3] = bytes32(proof.sortedAccum.y_0);
-        round1[4] = bytes32(proof.sortedAccum.y_1);
-        round1[5] = bytes32(proof.w4.x_0);
-        round1[6] = bytes32(proof.w4.x_1);
-        round1[7] = bytes32(proof.w4.y_0);
-        round1[8] = bytes32(proof.w4.y_1);
+        round1[1] = bytes32(proof.lookupReadCounts.x_0);
+        round1[2] = bytes32(proof.lookupReadCounts.x_1);
+        round1[3] = bytes32(proof.lookupReadCounts.y_0);
+        round1[4] = bytes32(proof.lookupReadCounts.y_1);
+        round1[5] = bytes32(proof.lookupReadTags.x_0);
+        round1[6] = bytes32(proof.lookupReadTags.x_1);
+        round1[7] = bytes32(proof.lookupReadTags.y_0);
+        round1[8] = bytes32(proof.lookupReadTags.y_1);
+        round1[9] = bytes32(proof.w4.x_0);
+        round1[10] = bytes32(proof.w4.x_1);
+        round1[11] = bytes32(proof.w4.y_0);
+        round1[12] = bytes32(proof.w4.y_1);
 
         beta = FrLib.fromBytes32(keccak256(abi.encodePacked(round1)));
         gamma = FrLib.fromBytes32(keccak256(abi.encodePacked(beta)));
@@ -109,17 +113,16 @@ library TranscriptLib {
         returns (Fr[NUMBER_OF_ALPHAS] memory alphas)
     {
         // Generate the original sumcheck alpha 0 by hashing zPerm and zLookup
-        // TODO(md): 5 post correct proof size fix
         uint256[9] memory alpha0;
         alpha0[0] = Fr.unwrap(previousChallenge);
-        alpha0[1] = proof.zPerm.x_0;
-        alpha0[2] = proof.zPerm.x_1;
-        alpha0[3] = proof.zPerm.y_0;
-        alpha0[4] = proof.zPerm.y_1;
-        alpha0[5] = proof.zLookup.x_0;
-        alpha0[6] = proof.zLookup.x_1;
-        alpha0[7] = proof.zLookup.y_0;
-        alpha0[8] = proof.zLookup.y_1;
+        alpha0[1] = proof.lookupInverses.x_0;
+        alpha0[2] = proof.lookupInverses.x_1;
+        alpha0[3] = proof.lookupInverses.y_0;
+        alpha0[4] = proof.lookupInverses.y_1;
+        alpha0[5] = proof.zPerm.x_0;
+        alpha0[6] = proof.zPerm.x_1;
+        alpha0[7] = proof.zPerm.y_0;
+        alpha0[8] = proof.zPerm.y_1;
 
         alphas[0] = FrLib.fromBytes32(keccak256(abi.encodePacked(alpha0)));
 
@@ -140,9 +143,9 @@ library TranscriptLib {
     function generateSumcheckChallenges(Honk.Proof memory proof, Fr prevChallenge)
         internal
         view
-        returns (Fr[LOG_N] memory sumcheckChallenges)
+        returns (Fr[CONST_PROOF_SIZE_LOG_N] memory sumcheckChallenges)
     {
-        for (uint256 i = 0; i < LOG_N; i++) {
+        for (uint256 i = 0; i < CONST_PROOF_SIZE_LOG_N; i++) {
             Fr[BATCHED_RELATION_PARTIAL_LENGTH + 1] memory univariateChal;
             univariateChal[0] = prevChallenge;
 
@@ -168,15 +171,11 @@ library TranscriptLib {
         rho = FrLib.fromBytes32(keccak256(abi.encodePacked(rhoChallengeElements)));
     }
 
-    function generateZMYChallenge(Fr previousChallenge, Honk.Proof memory proof)
-        internal
-        view
-        returns (Fr zeromorphY)
-    {
-        uint256[LOG_N * 4 + 1] memory zmY;
+    function generateZMYChallenge(Fr previousChallenge, Honk.Proof memory proof) internal view returns (Fr zeromorphY) {
+        uint256[CONST_PROOF_SIZE_LOG_N * 4 + 1] memory zmY;
         zmY[0] = Fr.unwrap(previousChallenge);
 
-        for (uint256 i; i < LOG_N; ++i) {
+        for (uint256 i; i < CONST_PROOF_SIZE_LOG_N; ++i) {
             zmY[1 + i * 4] = proof.zmCqs[i].x_0;
             zmY[2 + i * 4] = proof.zmCqs[i].x_1;
             zmY[3 + i * 4] = proof.zmCqs[i].y_0;
